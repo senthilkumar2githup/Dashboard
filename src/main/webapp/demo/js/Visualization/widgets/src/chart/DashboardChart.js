92514a18c4246576715239ec6284adaa1a1c4c1e
dhanasiddharth <dhanasiddharth@gmail.com>
2014-12-19 18:20:13 +0530

    Updated Json parsing

--- src/main/webapp/demo/js/Visualization/widgets/src/chart/DashboardChart.js
+++ src/main/webapp/demo/js/Visualization/widgets/src/chart/DashboardChart.js
@@ -14,17 +14,17 @@
 		
 		var myData = actualData;
 		var subject = ["subject"];
-		var column = myData[0]['data']['rows'][0];
+		var column = myData['data']['rows'][0];
 		var subjectwithColumn = subject.concat(column); 			
 		this.columns(subjectwithColumn);
 			
         if (chartType == 'pie')
 		{
 			var pieChartData=[];			
-			for(i = 0; i<myData[0]['data']['rows'][0].length; i++)	{	
+			for(i = 0; i<myData['data']['rows'][0].length; i++)	{	
 				var newData=[];
-				newData[0] = myData[0]['data']['rows'][0][i];
-				newData[1] = myData[0]['data']['rows'][1][i];						
+				newData[0] = myData['data']['rows'][0][i];
+				newData[1] = myData['data']['rows'][1][i];						
 				pieChartData[i] = newData;
 			}			
 			this.data(pieChartData);
@@ -33,11 +33,11 @@
 		if (chartType == 'line' || chartType == 'bar')
 		{	
 			var ChartData=new Array();			 
-			for (i=0;i<myData[0]['axis']['x']['categories'].length;i++)
+			for (i=0;i<myData['axis']['x']['categories'].length;i++)
 			{				
 				var columnSubject = [];
-				columnSubject[0] = myData[0]['axis']['x']['categories'][i];				
-				var columnInfo = myData[0]['data']['rows'][i+1];				
+				columnSubject[0] = myData['axis']['x']['categories'][i];				
+				var columnInfo = myData['data']['rows'][i+1];				
 				ChartData[i]=columnSubject.concat(columnInfo);				
 			}
 			this.data(ChartData);
